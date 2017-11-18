714fe164011610a10d6683cc088619470eb257b9
yanxiaojun617 <yanxiaojun617@163.com>
2017-11-18 17:11:30 +0800

    refactor:

--- providers/config.js
+++ providers/config.js
@@ -4,9 +4,9 @@
  */
 const config = {
   DEBUG:true,
-  APP_SERVE_URL: 'http://productv2.dn168.net/api/',
+  APP_SERVE_URL: 'http://productv2.dn.net/api/',
   // APP_SERVE_URL: 'http://88.128.18.144:8080/wxa/api/',
-  FILE_SERVE_URL: 'http://product.dn168.net/file/',
+  FILE_SERVE_URL: 'http://product.dn.net/file/',
   PAGE_SIZE: 5
 };
 
